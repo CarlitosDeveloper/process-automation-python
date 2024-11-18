2ab725cfca3d4c13578a09b4475dbdd92f1e1cbe
LearnWithCarlitos <LearnWithCarlitos@gmail.com>
2024-11-17 19:46:43 -0500

    change order options

--- menu_project.bat
+++ menu_project.bat
@@ -6,13 +6,13 @@ echo Seleccione el comando que desea ejecutar:
 echo ================================
 echo 0. Salir
 echo 1. Delete repeat in all sub folders - Picture
-echo 2. Delete repeat in all sub folders - Video
-echo 3. Change format in all sub folders - Picture
-echo 4. Change format in all sub folders - Video
-echo 5. Detect repeat in all sub folders - Picture (percentage)
-echo 6. Detect (folders, Sub folders, Files).
-echo 7. Detect (folders, Sub folders).
-echo 8. Change format in all sub folders - Picture (JPG)
+echo 2. Detect repeat in all sub folders - Picture (Percentage)
+echo 3. Change format in all sub folders - Picture (JPG)
+echo 4. Change format in all sub folders - Picture (JPEG)
+echo 5. Delete repeat in all sub folders - Video
+echo 6. Change format in all sub folders - Video
+echo 7. Detect (Folders, Sub folders, Files).
+echo 8. Detect (Folders, Sub folders).
 echo 9. Charge All files (.py) in (.exe)
 
 echo ================================
@@ -24,19 +24,19 @@ if "%opcion%"=="0" (
 ) else if "%opcion%"=="1" (
     python src\apps\Delete_Repeat_Picture.py
 ) else if "%opcion%"=="2" (
-    python src\apps\Delete_Repeat_Video.py
-) else if "%opcion%"=="3" (
-    python src\apps\Change_Format_Picture.py
-) else if "%opcion%"=="4" (
-    python src\apps\Change_Format_Video.py
-) else if "%opcion%"=="5" (
     python src\apps\Detect_Repeat_Images_By_Percentage.py
-) else if "%opcion%"=="6" (
-    python src\apps\Draw_Folder_Layout_Files.py
-) else if "%opcion%"=="7" (
-    python src\apps\Draw_Folder_Layout.py
-) else if "%opcion%"=="8" (
+) else if "%opcion%"=="3" (
     python src\apps\Change_Format_Picture_JPG.py
+) else if "%opcion%"=="4" (
+    python src\apps\Change_Format_Picture_JPEG.py
+) else if "%opcion%"=="5" (
+    python src\apps\Delete_Repeat_Video.py
+) else if "%opcion%"=="6" (
+    python src\apps\Change_Format_Video.py
+) else if "%opcion%"=="7" (
+    python src\apps\Draw_Folder_Layout_Files.py
+) else if "%opcion%"=="8" (
+    python src\apps\Draw_Folder_Layout.py
 ) else if "%opcion%"=="9" (
     python Create_Executables.py
 ) else (
